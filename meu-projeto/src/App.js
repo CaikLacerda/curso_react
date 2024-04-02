727ae050b83d31c97f9a8b0f42b7097ee1a76e24
Caik D. Lacerda <caiklacerda9@gmail.com>
2024-04-02 17:16:26 -0300

    Criacao do app para o projeto

---
+++ meu-projeto/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ola Mundo!</h1>
+      <p>Meu primeiro App :D</p>
+    </div>
+  );
+}
+
+export default App;
