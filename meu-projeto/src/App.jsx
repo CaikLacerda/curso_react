42a2cdcbf3462c8dc242e804402315630eaa8b87
Caik D. Lacerda <caiklacerda9@gmail.com>
2024-04-11 12:33:20 -0300

    aula sobre css

--- meu-projeto/src/App.jsx
+++ meu-projeto/src/App.jsx
@@ -2,13 +2,16 @@ import './App.css';
 import HelloWorld from './components/HelloWorld';
 import SayMyName from './components/SayMyName';
 import Pessoa from './components/pessoa';
+import Frase from './components/Frase';
 
 
 function App() {
 
   return (
     <div className="App">
-    
+      
+      <Frase />
+      <Frase />
       <SayMyName nome="Caik"/>
       <SayMyName nome="Isabela"/>
       <Pessoa 
--- meu-projeto/src/components/Frase.jsx
+++ meu-projeto/src/components/Frase.jsx
@@ -1,7 +1,9 @@
+import styles from './Frase.module.css'
+
 function Frase() {
     return (
-        <div>
-            <p>Importei um componente pelo HelloWorld!</p>
+        <div className={styles.fraseContainer}>
+            <p className={styles.fraseContent}>Importei um componente pelo HelloWorld!</p>
         </div>
     )
 }
