96132c55da2631e19b8f2c587b12b492d944a449
Caik D. Lacerda <caiklacerda9@gmail.com>
2024-04-15 17:50:45 -0300

    useState

--- meu-projeto/src/components/Form.jsx
+++ meu-projeto/src/components/Form.jsx
@@ -1,16 +1,38 @@
+import { useState } from 'react'
+
 function Form() {
 
     function cadastrarUsuario(e) {
         e.preventDefault() // Usa-se este comando para prevenir a funcionalidade padrao do HTML de enviar o formulario, sem que possa ser mostrado no console o codigo abaixo
-        console.log("Cadastrou o usuario")
+        console.log(`Usuario ${name} foi cadastrado com a senha ${password}`)
     }
+
+    const [name, setName] = useState()
+    const [password, setPassword] = useState()
 
     return (
         <div>
             <h1>Meu cadastro</h1>
             <form onSubmit={cadastrarUsuario}>
                 <div>
-                    <input type="text" placeholder="Digite o seu nome"/>
+                    <label htmlFor="name">Nome:</label>
+                    <input 
+                        type="text" 
+                        id="name" 
+                        name="name" 
+                        placeholder="Digite o seu nome"
+                        onChange={(e) => setName(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="password">Senha:</label>
+                    <input 
+                        type="password" 
+                        id="password" 
+                        name="password" 
+                        placeholder="Digite a sua senha"
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </div>
                 <div>
                     <input type="submit" value="Cadastrar" />
